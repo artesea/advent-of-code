940cfe75eea2be15eecd89290b12ee1af0d3a779
Ryan Cullen <ryancullen@artesea.co.uk>
2021-12-24 15:53:28 +0000

    Update 19a.php
    Added additional checks which helped find the bug, -$x in the first position for the final two rotations instead of -$z.
    Part 2 also added.

--- 2021/19a.php
+++ 2021/19a.php
@@ -3,7 +3,7 @@ $mcstart = microtime(true);
 echo "<pre>\n";
 $input = trim(str_replace("\r","",file_get_contents("19.txt")));
 
-//too high 371
+//too high 371, 368
 
 $lines = explode("\n", $input);
 $id = -1;
@@ -14,6 +14,7 @@ foreach($lines as $line) {
 	}
 	else if(trim($line) !== "") {
 		$scanner[$id][] = explode(",",$line);
+		$strings[$id][] = $line;
 	}
 }
 //print_r($scanner);
@@ -44,7 +45,7 @@ $toscan[] = 0;
 $found[] = 0;
 
 while(sizeof($toscan)) {
-	$i = array_shift($toscan);
+	$i = array_pop($toscan);
 	for($j=0;$j<sizeof($diffs);$j++) {
 		if(!in_array($j,$found)) {
 			$union = array_intersect($diffs[$i], $diffs[$j]);
@@ -63,6 +64,7 @@ while(sizeof($toscan)) {
 					$temp["{$key_ib}|{$key_jb}"]++;
 					//echo $u . "\t" . implode(",",$diffs_lib[$i][$u][0]) . "\t" . implode(",",$diffs_lib[$i][$u][1]) . "\t=>\t" . implode(",",$diffs_lib[$j][$u][0]) . "\t" . implode(",",$diffs_lib[$j][$u][1]) . "\n";
 				}
+				
 				//print_r($temp);
 				foreach($temp as $t => $v) {
 					if($v>=11) { //the cord matched with with 11 others
@@ -80,50 +82,69 @@ while(sizeof($toscan)) {
 					}
 				}
 				//print_r($offsets);
-				$rot = 0;
+				$rot = -1;
 				foreach($offsets as $r => $v) {
 					if(sizeof($v) == 1) {
 						$rot = $r;
 						$off = explode(",",key($v));
 					}
 				}
-				//rotate and move found item
-				echo "Rotation {$rot} needed, and offset is " . implode(",",$off) . "\n";
-				$loc[$j] = $off;
-				//print_r($scanner[$j]);
-				for($t=0;$t<sizeof($scanner[$j]);$t++) {
-					$scan = $scanner[$j][$t];
-					$rots = rotations($scan);
-					$new  = [$rots[$rot][0]+$loc[$j][0],$rots[$rot][1]+$loc[$j][1],$rots[$rot][2]+$loc[$j][2]];
-					$scanner[$j][$t] = $new;
+				if($rot == -1) {
+					echo "<strong>Didn't find a rotation!</strong>\n";
 				}
-				//print_r($scanner[$j]);
-				//rebuild diffs lib (sure we could do something better than this)
-				for($x=0;$x<sizeof($scanner[$j]);$x++) {
-					for($y=$x+1;$y<sizeof($scanner[$j]);$y++) {
-						$a = $scanner[$j][$x];
-						$b = $scanner[$j][$y];
-						$distance = ($b[0]-$a[0])**2 + ($b[1]-$a[1])**2 + ($b[2]-$a[2])**2;
-						$diffs_lib[$j][$distance] = [$a,$b];
+				else {
+					//rotate and move found item
+					echo "Rotation {$rot} needed, and offset is " . implode(",",$off) . "\n";
+					$loc[$j] = $off;
+					//print_r($scanner[$j]);
+					for($t=0;$t<sizeof($scanner[$j]);$t++) {
+						$scan = $scanner[$j][$t];
+						$rots = rotations($scan);
+						$new  = [$rots[$rot][0]+$loc[$j][0],$rots[$rot][1]+$loc[$j][1],$rots[$rot][2]+$loc[$j][2]];
+						$scanner[$j][$t] = $new;
+						$strings[$j][$t] = implode(",",$new);
 					}
+					//print_r($scanner[$j]);
+					//rebuild diffs lib (sure we could do something better than this)
+					for($x=0;$x<sizeof($scanner[$j]);$x++) {
+						for($y=$x+1;$y<sizeof($scanner[$j]);$y++) {
+							$a = $scanner[$j][$x];
+							$b = $scanner[$j][$y];
+							$distance = ($b[0]-$a[0])**2 + ($b[1]-$a[1])**2 + ($b[2]-$a[2])**2;
+							$diffs_lib[$j][$distance] = [$a,$b];
+						}
+					}
+					$found[] = $j;
+					$toscan[] = $j;
+					$uni = array_intersect($strings[$i],$strings[$j]);
+					//print_r($uni);
+					echo "Now have " . sizeof($uni) . " intersections\n";
 				}
-				$found[] = $j;
-				$toscan[] = $j;
 			}
 		}
 	}
 }
 //finally loop through all the scanners and save unique beacons
 $unique = [];
-foreach($scanner as $scan) {
+foreach($strings as $id => $scan) {
 	foreach($scan as $beacon) {
-		$k = implode(",",$beacon);
-		if(!in_array($k,$unique)) $unique[] = $k;
+		//$unique[$beacon][] = $id;
+		if(!in_array($beacon,$unique)) $unique[] = $beacon;
 	}
 }
-print_r($unique);
-echo "Beacons " . sizeof($unique) . "\n\n";
+//$unique = array_unique($unique);
+//print_r($unique);
+echo "\n\nBeacons: " . sizeof($unique) . "\n";
 //print_r($loc);
+
+$max = 0;
+for($a=0;$a<sizeof($loc);$a++) {
+	for($b=$a+1;$b<sizeof($loc);$b++) {
+		$manhattan = abs($loc[$a][0]-$loc[$b][0]) + abs($loc[$a][1]-$loc[$b][1]) + abs($loc[$a][2]-$loc[$b][2]);
+		$max = max($max,$manhattan);
+	}
+}
+echo "Max Manhattan: {$max}\n";
 
 function rotations($beacon) {
 	[$x,$y,$z] = $beacon;
@@ -155,8 +176,8 @@ function rotations($beacon) {
 		
 		[-$z,-$y,-$x],
 		[-$z,-$x, $y],
-		[-$x, $y, $x],
-		[-$x, $x,-$y],
+		[-$z, $y, $x],
+		[-$z, $x,-$y],
 	];
 }
 

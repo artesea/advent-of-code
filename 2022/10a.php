5fb589128532bec1b99f6cd378beb42468bd7584
Ryan Cullen <ryancullen@artesea.co.uk>
2022-12-10 14:29:34 +0000

    Create 10a.php

---
+++ 2022/10a.php
@@ -0,0 +1,33 @@
+<?php
+$mcstart = microtime(true);
+echo "<pre>\n";
+$input = explode("\n",(str_replace("\r","",file_get_contents("10.txt"))));
+$part1 = $part2 = "";
+
+$cycle = 0;
+$x = 1;
+$part1 = 0;
+
+foreach($input as $line) {
+	for($z=1;$z<=($line=="noop"?1:2);$z++) {
+		$cycle++;
+		echo "{$cycle}\t{$x}";
+		if($cycle%40 == 20) {
+			$sig = $cycle*$x;
+			echo "\t{$sig}\n";
+			$part1+=$sig;
+		}
+		else {
+			echo "\n";
+		}
+	}
+	if(preg_match("#addx (\-?\d+)#",$line,$v)) {
+		$x+=$v[1];
+	}
+}
+
+echo "\n\nPart 1: " . $part1;
+echo "\n\nPart 2: " . $part2;
+
+$mcdiff = microtime(true) - $mcstart;
+echo "\n\nTime: {$mcdiff}";
